5b95920109730a50aee51f2032c796287e45d0d4
Pierre Poissinger <pierre.poissinger@gmail.com>
2025-12-18 13:42:09 +0100

    Removed S1 debs

--- embedded_files.go
+++ embedded_files.go
@@ -4,5 +4,5 @@ import (
 	"embed"
 )
 
-//go:embed "config.json" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_ALGO_APP-403.deb" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_client_deb-129.deb" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_s1_client_deb-200.deb" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_s1_test-201.deb" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_test-129.deb"
+//go:embed "config.json" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_ALGO_APP-403.deb" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_client_deb-129.deb" "artillery-m1-debs/Yuntu_m1-Yuntu_m1_test-129.deb"
 var embeddedFiles embed.FS
